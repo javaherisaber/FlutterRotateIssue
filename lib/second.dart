f1dec3df1c4216c1af791975152e3a4dc1292de9
Mahdi Javaheri Saber <m.javaherisaber@gmail.com>
2022-01-05 10:53:24 +0330

    Init git

---
+++ lib/second.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Screen')),
+      body: Center(child: Text('This is second screen')),
+    );
+  }
+}
